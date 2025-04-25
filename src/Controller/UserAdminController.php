dba135cb96227b155ee4bfbcf3b06abc6ce2c652
Jasmin Medved <jmedved85@gmail.com>
2025-04-25 12:50:02 +0200

    phpstan & php-cs-fixer install, phpstan fix (lvl 6), cs fix

--- src/Controller/UserAdminController.php
+++ src/Controller/UserAdminController.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Sonata\AdminBundle\Controller\CRUDController;
 
+/**
+ * @phpstan-extends CRUDController<User>
+ */
 final class UserAdminController extends CRUDController
 {
 }
